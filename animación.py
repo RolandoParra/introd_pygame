4267484eef6d773f995cb7b2ec6fe89d9da3a8be
RolandoParra <rolandoparra@colegioguanenta.edu.co>
2026-05-06 08:24:10 -0500

    carbón

--- animación.py
+++ animación.py
@@ -1,6 +1,7 @@
 # importamos la libreria pygame
 import pygame
 import sys
+import random
 
 # inicializamos los modulos de la librería
 pygame.init()
@@ -14,10 +15,12 @@ pygame.display.set_caption("Rebotes rectángulo")
 # definición colores
 rojo = (255,0,0)
 azul = (0,0,255)
+rancol = (0, 0, 0)
 
 # variable de movimiento
 XX = 300
-MOVIMIENTO = 3
+YY = 300
+MOVIMIENTO = 9
 
 # Objeto para la gestión del tiempo
 clock = pygame.time.Clock()
@@ -34,18 +37,32 @@ while True:
 
     ventana.fill(azul)
 
+    rancol = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
     # movimiento del rectángulo
-    XX = XX + MOVIMIENTO
+    XX = random.randint(10, 320) + MOVIMIENTO
+    YY = random.randint(10, 320) + MOVIMIENTO
     # esto cambia la dirección, haciendo que rebote :3
     if XX >= 320:
         XX = 320
-        MOVIMIENTO = -3
+        MOVIMIENTO = -9
     elif XX <= 0:
         XX = 0
-        MOVIMIENTO = 3
+        MOVIMIENTO = 9
+
+    if XX >= 320:
+        XX = 320
+        MOVIMIENTO = -9
+    elif XX <= 0:
+        XX = 0
+        MOVIMIENTO = 9
 
     # dibujar rectangulo en ventana
-    pygame.draw.rect(ventana, rojo, (XX,100,80,80))
+    pygame.draw.rect(ventana, rancol, (XX, YY, 80, 80))
+    pygame.draw.rect(ventana, rancol, (XX, YY, 80, 80))
+    pygame.draw.rect(ventana, rancol, (XX, YY, 80, 80))
+    pygame.draw.rect(ventana, rancol, (XX, YY, 80, 80))
+    pygame.draw.rect(ventana, rancol, (XX, YY, 80, 80))
 
     # actualizar visualización de la ventana
     pygame.display.flip()
